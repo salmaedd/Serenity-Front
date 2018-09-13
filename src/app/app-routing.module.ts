dc67dee4b2bb83819516d9c5432748923e3ec400
EL ALAMI MOUAAD <mouaad.el-alami@capgemini.com>
2018-09-13 13:56:42 +0100

    added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { DisplayUserComponent } from './components/administration/display-user/display-user.component';
+import { AddUserComponent } from './components/administration/add-user/add-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:DashboardComponent
+  },
+  {
+    path:"administration/users",
+    component:DisplayUserComponent
+  },
+  {
+    path:"administration/adduser",
+    component: AddUserComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './components/administration/add-user/add-user.component';
+import { UserService } from './services/administration/user.service';
+import { HttpModule } from '@angular/http';
+import { DisplayUserComponent } from './components/administration/display-user/display-user.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    DisplayUserComponent,
+    NavbarComponent,
+    DashboardComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
